e2b42d676f808597f14879e16de3f7c6c35db4fc
geksanit <geksanit.w@gmail.com>
2020-11-21 22:04:34 +0700

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Location = new System.Drawing.Point(407, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "нарисовать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Draw);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.AInputOnChange);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(221, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.BInputOnChange);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(164, 71);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(159, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "A + B";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 139);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(593, 396);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Операции на множествами";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(123, 263);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "A";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(469, 263);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "B";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(84, 104);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(10, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Enabled = false;
+            this.label3.Location = new System.Drawing.Point(415, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "U";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(436, 21);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(169, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 547);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Диграммы Эйлера-Венна";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Graphics holst;
+        private Pen pen_green;
+        private Point A_center;
+        private Point B_center;
+        private int radius;
+        private ISet<Char> A_set;
+        private ISet<Char> B_set;
+        private ISet<Char> U_set;
+        private GraphicsPath A_path;
+        private GraphicsPath B_path;
+
+        public Form1()
+        {
+            InitializeComponent();
+            A_center = new Point(200, 200);
+            B_center = new Point(400, 200);
+            radius = 130;
+            pen_green = new Pen(Color.Green, 1);
+            A_set = new HashSet<char>();
+            A_set.Add('x');
+            A_set.Add('x');
+            A_set.Add('y');
+            A_set.Add('z');
+
+            B_set = new HashSet<char>();
+            B_set.Add('y');
+            B_set.Add('m');
+
+            U_set = new HashSet<char>();
+            U_set.UnionWith(A_set);
+            U_set.UnionWith(B_set);
+            U_set.Add('u');
+
+            textBox1.Text = SetToString(A_set);
+            textBox2.Text = SetToString(B_set);
+            textBox3.Text = SetToString(U_set);
+
+            A_path = GetPathForCircle(A_center);
+            B_path = GetPathForCircle(B_center);
+        }
+
+        public void Draw(object sender, EventArgs e)
+        {
+            Console.WriteLine("Draw call {0}", textBox4.Text);
+            var res = ParseExpression(textBox4.Text);
+            Console.WriteLine("res {0}", SetToString(res));
+            label10.Text = SetToString(res);
+            renderResult(res);
+        }
+
+        private ISet<char> ParseExpression(string str)
+        {
+            string patternBinExpr = @"(!?\w)\s*(\+|\*|\\)\s*(!?\w)"; // example !A + B
+            Match match = Regex.Match(str, patternBinExpr);
+            if (match.Success)
+            {
+                var x = ParseSymbol(match.Groups[1].Value);
+                var oper = match.Groups[2].Value;
+                var y = ParseSymbol(match.Groups[3].Value);
+                if (oper == "+")
+                {
+                    x.UnionWith(y);
+                    return x;
+                }
+                else if (oper == "*")
+                {
+                    x.IntersectWith(y);
+                    return x;
+                }
+                x.ExceptWith(y);
+                return x;
+            }
+            string patternUnarExpr = @"(!?\w)";
+            Match matchUnar = Regex.Match(str, patternUnarExpr);
+            if (matchUnar.Success)
+            {
+                var x = ParseSymbol(matchUnar.Groups[1].Value);
+                return x;
+            }
+            return new HashSet<char>();
+        }
+
+        private ISet<char> ParseSymbol(string str) 
+        {
+            string pattern = @"(!?)(A|B)"; // example !A
+            Match match = Regex.Match(str, pattern);
+            if (match.Success)
+            {
+                var symbol = match.Groups[2].Value;
+                var set = symbol == "A" ? A_set : B_set;
+                if (match.Groups[1].Value == String.Empty)
+                {
+                    return new HashSet<char>(set);
+                }
+                else
+                {
+                    
+                    var res = new HashSet<char>();
+                    res.UnionWith(U_set);
+                    res.ExceptWith(set);
+                    return res;
+                }
+            }
+            else
+            {
+                return new HashSet<char>();
+            }
+        }
+
+        private string SetToString(ISet<char> set)
+        {
+            var str = new StringBuilder();
+            foreach (char ch in set)
+            {
+                str.Append(ch);
+                str.Append(' ');
+            }
+            return str.ToString();
+        }
+
+        private bool isPointInCircle(Point point, Point center)
+        {
+            var a = point.X - center.X;
+            var b = point.Y - center.Y;
+            var c = Math.Sqrt(a * a + b * b);
+            return c < radius;
+        }
+
+        private void renderResult(ISet<char> set)
+        {
+            holst = Graphics.FromHwnd(pictureBox1.Handle);
+            ClearCanvas();
+            var w = pictureBox1.Width;
+            var h = pictureBox1.Height;
+
+            Rectangle originalRectangle = new Rectangle(0, 0, w, h);
+            Region resultRegion = new Region(originalRectangle);
+            resultRegion.MakeEmpty();
+
+            foreach (char ch in set)
+            {
+                Region region = new Region(originalRectangle);
+                if (A_set.Contains(ch))
+                {
+                    region.Intersect(A_path);
+                } else
+                {
+                    region.Exclude(A_path);
+                }
+                if (B_set.Contains(ch))
+                {
+                    region.Intersect(B_path);
+                }
+                else
+                {
+                    region.Exclude(B_path);
+                }
+                resultRegion.Union(region);
+            }
+            SolidBrush myBrush = new SolidBrush(Color.Blue);
+            holst.FillRegion(myBrush, resultRegion);
+        }
+
+        public void RenderCircle(Point center)
+        {
+            holst.DrawEllipse(pen_green, new Rectangle(center.X - radius, center.Y - radius, radius * 2, radius * 2));
+        }
+
+        private GraphicsPath GetPathForCircle(Point center)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(center.X - radius, center.Y - radius, radius * 2, radius * 2);
+            return path;
+        }
+
+        private void ClearCanvas()
+        {
+            holst.Clear(Color.White);
+            RenderCircle(A_center);
+            RenderCircle(B_center);
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            holst = e.Graphics;
+            ClearCanvas();
+        }
+
+        private void UpdateUSet()
+        {
+            U_set = new HashSet<char>();
+            U_set.UnionWith(A_set);
+            U_set.UnionWith(B_set);
+            U_set.Add('u');
+            textBox3.Text = SetToString(U_set);
+        }
+
+        private void UpdateSetFromText(ISet<char> set, string text)
+        {
+            set.Clear();
+            var textWithoutSpaces = text.Replace(" ", "");
+            foreach (char ch in textWithoutSpaces)
+            {
+                set.Add(ch);
+            }
+        }
+
+        private void AInputOnChange(object sender, EventArgs e)
+        {
+            UpdateSetFromText(A_set, textBox1.Text);
+            UpdateUSet();
+        }
+
+        private void BInputOnChange(object sender, EventArgs e)
+        {
+            UpdateSetFromText(B_set, textBox2.Text);
+            UpdateUSet();
+        }
+
+    }
+    }
